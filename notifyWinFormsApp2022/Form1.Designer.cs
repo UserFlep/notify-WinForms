4fad7bc94fe4f3173849088e2c9f896c2210ec74
UserFlep <era.user.ivan@gmail.com>
2022-07-17 22:36:23 +0300

    initial commit

---
+++ notifyWinFormsApp2022/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace notifyWinFormsApp2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.lblDBCon = new System.Windows.Forms.Label();
+            this.txtBoxHostname = new System.Windows.Forms.TextBox();
+            this.txtBoxPort = new System.Windows.Forms.TextBox();
+            this.txtBoxUsername = new System.Windows.Forms.TextBox();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.lblHostname = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblDBName = new System.Windows.Forms.Label();
+            this.txtBoxDBName = new System.Windows.Forms.TextBox();
+            this.lblTableName = new System.Windows.Forms.Label();
+            this.txtBoxTableName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(14, 179);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowTemplate.Height = 25;
+            this.dgvData.Size = new System.Drawing.Size(638, 290);
+            this.dgvData.TabIndex = 0;
+            // 
+            // lblDBCon
+            // 
+            this.lblDBCon.AutoSize = true;
+            this.lblDBCon.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDBCon.Location = new System.Drawing.Point(13, 13);
+            this.lblDBCon.Name = "lblDBCon";
+            this.lblDBCon.Size = new System.Drawing.Size(135, 17);
+            this.lblDBCon.TabIndex = 1;
+            this.lblDBCon.Text = "Подключение к БД:";
+            // 
+            // txtBoxHostname
+            // 
+            this.txtBoxHostname.Location = new System.Drawing.Point(75, 41);
+            this.txtBoxHostname.Name = "txtBoxHostname";
+            this.txtBoxHostname.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxHostname.TabIndex = 2;
+            this.txtBoxHostname.Text = "localhost";
+            // 
+            // txtBoxPort
+            // 
+            this.txtBoxPort.Location = new System.Drawing.Point(252, 41);
+            this.txtBoxPort.Name = "txtBoxPort";
+            this.txtBoxPort.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxPort.TabIndex = 3;
+            this.txtBoxPort.Text = "5432";
+            // 
+            // txtBoxUsername
+            // 
+            this.txtBoxUsername.Location = new System.Drawing.Point(75, 73);
+            this.txtBoxUsername.Name = "txtBoxUsername";
+            this.txtBoxUsername.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxUsername.TabIndex = 4;
+            this.txtBoxUsername.Text = "postgres";
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(252, 73);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxPassword.TabIndex = 5;
+            this.txtBoxPassword.Text = "root";
+            // 
+            // lblHostname
+            // 
+            this.lblHostname.AutoSize = true;
+            this.lblHostname.Location = new System.Drawing.Point(13, 44);
+            this.lblHostname.Name = "lblHostname";
+            this.lblHostname.Size = new System.Drawing.Size(62, 15);
+            this.lblHostname.TabIndex = 6;
+            this.lblHostname.Text = "Hostname";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(184, 44);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(29, 15);
+            this.lblPort.TabIndex = 7;
+            this.lblPort.Text = "Port";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(13, 76);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(60, 15);
+            this.lblUsername.TabIndex = 8;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(183, 76);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(57, 15);
+            this.lblPassword.TabIndex = 9;
+            this.lblPassword.Text = "Password";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(388, 41);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 96);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(523, 41);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(129, 96);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = false;
+            // 
+            // lblDBName
+            // 
+            this.lblDBName.AutoSize = true;
+            this.lblDBName.Location = new System.Drawing.Point(12, 108);
+            this.lblDBName.Name = "lblDBName";
+            this.lblDBName.Size = new System.Drawing.Size(55, 15);
+            this.lblDBName.TabIndex = 13;
+            this.lblDBName.Text = "DB name";
+            // 
+            // txtBoxDBName
+            // 
+            this.txtBoxDBName.Location = new System.Drawing.Point(75, 105);
+            this.txtBoxDBName.Name = "txtBoxDBName";
+            this.txtBoxDBName.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxDBName.TabIndex = 12;
+            this.txtBoxDBName.Text = "notify";
+            // 
+            // lblTableName
+            // 
+            this.lblTableName.AutoSize = true;
+            this.lblTableName.Location = new System.Drawing.Point(183, 108);
+            this.lblTableName.Name = "lblTableName";
+            this.lblTableName.Size = new System.Drawing.Size(68, 15);
+            this.lblTableName.TabIndex = 15;
+            this.lblTableName.Text = "Table name";
+            // 
+            // txtBoxTableName
+            // 
+            this.txtBoxTableName.Location = new System.Drawing.Point(252, 105);
+            this.txtBoxTableName.Name = "txtBoxTableName";
+            this.txtBoxTableName.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxTableName.TabIndex = 14;
+            this.txtBoxTableName.Text = "datas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 481);
+            this.Controls.Add(this.lblTableName);
+            this.Controls.Add(this.txtBoxTableName);
+            this.Controls.Add(this.lblDBName);
+            this.Controls.Add(this.txtBoxDBName);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblHostname);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.txtBoxUsername);
+            this.Controls.Add(this.txtBoxPort);
+            this.Controls.Add(this.txtBoxHostname);
+            this.Controls.Add(this.lblDBCon);
+            this.Controls.Add(this.dgvData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label lblDBCon;
+        private System.Windows.Forms.TextBox txtBoxHostname;
+        private System.Windows.Forms.TextBox txtBoxPort;
+        private System.Windows.Forms.TextBox txtBoxUsername;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.Label lblHostname;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblDBName;
+        private System.Windows.Forms.TextBox txtBoxDBName;
+        private System.Windows.Forms.Label lblTableName;
+        private System.Windows.Forms.TextBox txtBoxTableName;
+    }
+}
+
---
+++ notifyWinFormsApp2022/Form1.cs
@@ -0,0 +1,72 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notifyWinFormsApp2022
+{
+    public partial class Form1 : Form
+    {
+        private string constring;
+        private NpgsqlConnection conn;
+        private string sql;
+        private NpgsqlCommand cmd;
+        private DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            constring = String.Format(
+                "Server={0};" +
+                "Port={1};" +
+                "User Id={2};" +
+                "Password={3};" +
+                "Database={4}",
+                txtBoxHostname.Text, 
+                txtBoxPort.Text, 
+                txtBoxUsername.Text,
+                txtBoxPassword.Text, 
+                txtBoxDBName.Text
+            );
+            conn = new NpgsqlConnection(constring);
+            try
+            {
+                conn.Open();
+                //conn.Notification += (o, e) => Console.WriteLine("Received notification");
+                
+                sql = @"select * from " + txtBoxTableName.Text;
+                cmd = new NpgsqlCommand(sql, conn);
+                var reader = cmd.ExecuteReader();
+                dt = new DataTable();
+                dt.Load(reader);
+                conn.Close();
+
+                dgvData.DataSource = null; //очистка
+                dgvData.DataSource = dt;
+                //while (true)
+                //{
+                 //   conn.Wait();   // Thread will block here
+                //}
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Error:" + ex);
+            }
+            finally
+            {
+                if(conn.State != ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+    }
+}
